049d05d7a61c17eb58c8950fa34dc75ea269ceba
justrishad777-code <justrishad.777@gmail.com>
2026-01-29 19:57:57 +0600

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,26 @@
+const navbarHTML = `
+<div class="menu-container">
+    <a href="notes.html" class="menu-card" style="border-left: 5px solid #ab47bc;">
+        <span class="card-title">💜 Secret Notes</span><span class="card-icon">📝</span>
+    </a>
+
+    <a href="routine.html" class="menu-card routine">
+        <span class="card-title">📅 Daily Routine</span><span class="card-icon">🌸</span>
+    </a>
+    <a href="quiz.html" class="menu-card quiz">
+        <span class="card-title">📝 Quiz Schedule</span><span class="card-icon">🌿</span>
+    </a>
+    <a href="assignment.html" class="menu-card assign">
+        <span class="card-title">📂 Assignments</span><span class="card-icon">💙</span>
+    </a>
+    <a href="mid.html" class="menu-card mid">
+        <span class="card-title">📖 Mid Term Exam</span><span class="card-icon">💜</span>
+    </a>
+    <a href="final.html" class="menu-card final">
+        <span class="card-title">🎓 Final Exam</span><span class="card-icon">❤️</span>
+    </a>
+</div>
+`;
+
+// === EI LINE TA TOMAR FILE E NAI, ETAI MAIN ===
+document.getElementById("navbar-placeholder").innerHTML = navbarHTML;
