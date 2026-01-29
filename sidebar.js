3c2ee820d2b60e892916e4f2abc860f8ed3ab5c6
justrishad777-code <justrishad.777@gmail.com>
2026-01-29 21:18:48 +0600

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,13 +1,10 @@
-// sidebar.js
-
 const sidebarHTML = `
 <div id="mySidenav" class="side-menu">
     <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
     <a href="index.html">🏠 Dashboard</a>
     
     <a href="notes.html">💜 Secret Notes</a>
-
-    <a href="routine.html">📅 Routine</a>
+    <a href="study.html">🌻 Study Plan</a> <a href="routine.html">📅 Routine</a>
     <a href="quiz.html">📝 Quiz</a>
     <a href="assignment.html">📂 Assignments</a>
     <a href="mid.html">📖 Mid Term</a>
